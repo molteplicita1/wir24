2da1c105d38f89377d08dae3c3dc034325e13637
tano <olivagaetano302@gmail.com>
2024-10-17 12:43:49 +0200

    now client.py makes only one query at a time

--- client.py
+++ client.py
@@ -1,66 +1,41 @@
-import json, time
-from functions import populate, pipeline
 from tokenizer import TokenizerFactory
-
-time_start = time.time()
-
-t1 = TokenizerFactory.create_tokenizer()
-t2 = TokenizerFactory.create_tokenizer(model="google/gemma-2-2b")
-
-tokenizers = [t1, t2]
-
-with open('config.json') as f:
-    config = json.load(f)
-
-chroma_address = config['chroma_address']
-chroma_port = config['chroma_port']
-chroma_collection = config['chroma_collection']
-data_path = config["data_path"]
-
-ollama_address = config["ollama_address"]
-ollama_port = config["ollama_port"]
-
-with open('embeddings.txt') as em:
-    embeddings = [e.strip() for e in em]
+from functions import *
+import os
 
 
-with open('models.txt') as mod:
-    models = [m.strip() for m in mod]
+with open('output.md', 'w') as f:
+    f.write("")
+    f.close()
+
+CHROMA_ADDRESS = os.getenv("CHROMA_ADDRESS")
+CHROMA_PORT = os.getenv("CHROMA_PORT")
+CHROMA_DB = os.getenv("CHROMA_DB")
+DATA_PATH = os.getenv("DATA_PATH")
+TOKENIZER = os.getenv("TOKENIZER")
+EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
+MODEL = os.getenv("MODEL")
+OLLAMA_ADDRESS = os.getenv("OLLAMA_ADDRESS")
+OLLAMA_PORT = os.getenv("OLLAMA_PORT")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--reset", action="store_true", help="Reset the database.")
+args = parser.parse_args()
+if args.reset:
+    print("✨ Clearing Database")
+    chromadb.HttpClient(host=CHROMA_ADDRESS, port=CHROMA_PORT).reset()
+    print("Database successfully cleaned! ✅")
+
+collection_name = f"{CHROMA_DB}"
+
+tokenizer = TokenizerFactory.create_tokenizer()
+
+populate(CHROMA_ADDRESS, CHROMA_PORT, collection_name, DATA_PATH, EMBEDDING_MODEL, OLLAMA_ADDRESS, OLLAMA_PORT, tokenizer)
+
+query = "Parlami del clustering che abbiamo studiato in Data Analytics"
+
+docs, source, distances = retrieve(CHROMA_ADDRESS, CHROMA_PORT, collection_name, EMBEDDING_MODEL, query, OLLAMA_ADDRESS, OLLAMA_PORT)
+
+do_query(query, docs, source, distances, OLLAMA_ADDRESS, OLLAMA_PORT, MODEL)
 
 
-with open('output.md', 'w') as out:
-    out.write("")
-    out.close()
-
-
-print("Output file cleared")
-
-with open('queries.txt') as quer:
-    queries = [q.strip("\n") for q in quer]
-
-for embedding_model in embeddings:
-    for tokenizer in tokenizers:
-        populate(chroma_address, chroma_port, chroma_collection, data_path, embedding_model, ollama_address, ollama_port, tokenizer)
-
-print("\n")
-
-with open('output.md', 'a') as f:
-    for tokenizer in tokenizers:
-        for query in queries:
-            print(f"\nQuery: {query}")
-            f.write(f"# QUERY: {query}\n")
-            f.flush()
-            for embedding_model in embeddings:
-                for model in models:
-                    print(f"Starting with {embedding_model}, {model} and {type(tokenizer).__name__}")
-                    f.write(f"## EMBEDDING: {embedding_model}\n")
-                    f.write(f"## MODEL: {model}\n")
-                    f.write(f"## TOKENIZER: {type(tokenizer).__name__}\n")
-                    f.flush()
-                    pipeline(chroma_address, chroma_port, chroma_collection, embedding_model, query, ollama_address, ollama_port, model)
-                    print(f"Done with {embedding_model}, {model} and {type(tokenizer).__name__}\n")
-        f.write("\n\n\n----------------------------------------\n\n\n")
-
-time_end = time.time()
-
-print(f"Total time: {time_end - time_start} seconds")
+print("\nDone!")
